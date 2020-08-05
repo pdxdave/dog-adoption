7aa1944bf95945e749607882b29bd9ef56dfad07
pdxdave <webdevdave@hotmail.com>
2020-08-05 11:26:29 -0700

    changes to main page

--- src/components/willowcreek.js
+++ src/components/willowcreek.js
@@ -16,7 +16,7 @@ const WillowCreek = () => {
                         <p className="willow-text text-muted" 
                             data-sal="slide-left" 
                             data-sal-delay="800" 
-                            data-sal-easing="ease">Nestled in the mountains of Maine, between Arch Point Lookout and Sandy Rock Cove is Willow Creek.  The home to <em>Willow Creek Dog Rescue & Adoption</em>.  Willow Creek started 11 years ago with the goal of rescuing at risk dogs in high kill shelters, strays and dogs that owners could no longer take care of.  We find them new homes. Along the way we educate the public about dog ownership and provide the tools and training to better prepare themselves to be good dog owners. </p>
+                            data-sal-easing="ease">Nestled in the mountains of Maine, between Arch Point Lookout and Sandy Rock Cove is Willow Creek.  The home to <em>Willow Creek Dog Rescue & Adoption</em>.  We started 11 years ago with the goal of rescuing at risk dogs in high kill shelters, strays and dogs that owners could no longer care for.  We find them new homes. Along the way we educate the public about dog ownership and provide the tools and training to better prepare themselves to be good dog owners.  All of our dogs are healthy, up-to-date with their vaccinations, and microchiped.  </p>
                     </Col>
                 </Row>
             </Container>
