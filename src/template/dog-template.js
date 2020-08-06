85e451b6e2ec06c1111d69db3b6fc36f30fc4d6f
pdxdave <webdevdave@hotmail.com>
2020-08-06 11:34:14 -0700

    more dog template changes

--- src/template/dog-template.js
+++ src/template/dog-template.js
@@ -46,10 +46,6 @@ const ComponentName = ({
                 </div>
             </div>{/* end of flex*/}
             </div>{/* end of wrapper*/}
-            <footer className="py-4" style={{textAlign: "center", borderTop: "1px solid gray", backgroundColor: "#f2f2f2"}}>
-                <span>Willow</span><img src={logo} alt="logo" style={{height: "100px", width: "100px"}}/><span>Creek</span>
-                <p>2436 Westbrook Drive, Monhagen, ME | Ph: 207-669-0325 | &copy; 2020</p>
-            </footer>
         </div>
         </Layout>
     )
